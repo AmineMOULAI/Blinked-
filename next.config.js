87786397fe22e11492235d7eaf392a4638c87cd2
AmineMOULAI <moulaiamine01@gmail.com>
2026-03-28 14:36:52 +0000

    Fix Vercel build errors (eslint and next config)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
-export default nextConfig;
+module.exports = nextConfig;
